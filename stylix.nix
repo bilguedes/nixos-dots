e9dc6301fc49163632724b0d561b99319d9744e2
bilguedes <guedesconfiabil@gmail.com>
2025-11-25 15:46:14 -0300

    stylix: change tokyonight-dark to catppuccin-mocha

--- stylix.nix
+++ stylix.nix
@@ -1,22 +1,21 @@
 { config, pkgs, ... }:
+
 {
-  stylix.enable = true;
-  
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
-
-  stylix.fonts.sansSerif = {
-    package = pkgs.noto-fonts-cjk-sans;
-    name = "Noto Sans CJK Sans";
-  };
-
-  stylix.fonts.emoji = {
-      package = pkgs.noto-fonts-color-emoji;
-      name = "Noto Color Emoji";
-  };
-
-  stylix.fonts.monospace = {
-    package = pkgs.nerd-fonts.iosevka;
-    name = "Iosevka NF";
-  };
-  stylix.image = ./walls/mountains.jpg;
+    stylix = {
+      enable = true;
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+      fonts.sansSerif = {
+        package = pkgs.noto-fonts-cjk-sans;
+        name = "Noto Sans CJK Sans";
+      };
+      fonts.emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+      fonts.monospace = {
+        package = pkgs.nerd-fonts.iosevka;
+        name = "Iosevka NF";
+      };
+      image = ./walls/mountains.jpg;
+    };
 }
